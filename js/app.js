f0c41b3e86eeb5089305ec93cc50a8ac4ed3fd5a
Iona Macbeth <ionalmacbeth@gmail.com>
2018-08-03 11:25:22 +0100

    js form set up

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  
+
+const form = document.querySelector('#new-item-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const travellist = document.querySelector('ul');
+  const newPlace = document.createElement('li');
+    const city = event.target.city.value;
+    const country = event.target.country.value;
+    const season = event.target.season.value;
+
+    newBook.textContent = `${city} ${country} ${season}`
+
+  bookslist.appendChild(newPlace);
+  form.reset();
+
+
+})
+
 })
